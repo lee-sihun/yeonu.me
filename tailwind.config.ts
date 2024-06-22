33322ce5c3a971e19df5c97bca9614be4d962ce1
lee-sihun <lee.sihun040223@gmail.com>
2024-06-22 13:38:32 +0900

    feat: 스크롤 애니메이션 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,24 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },                   
+    },
+  },
   plugins: [],
 };
 export default config;
