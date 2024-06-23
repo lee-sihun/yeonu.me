b296a0fa69fdad3bd48865177c3df70cd6c2b696
lee-sihun <lee.sihun040223@gmail.com>
2024-06-23 10:24:23 +0900

    feat: About 섹션 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Title from "@/components/Title";
 import Gallery from "@/components/Gallery";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <>
       <Title />
       <Gallery />
+      <About />
     </>
   );
 }
