33529f4abed6249c28c39c4edeaa953b41edba80
lee-sihun <lee.sihun040223@gmail.com>
2024-06-23 22:04:43 +0900

    feat: 년도별 스킬 리스트

---
+++ components/SkillsList/skills.ts
@@ -0,0 +1,41 @@
+import explorer from "@/img/explorer.png";
+import paint from "@/img/paint.png";
+import photoshop from "@/img/photoshop.png";
+import premiere from "@/img/premiere.png";
+import after from "@/img/aftereffect.png";
+import chrome from "@/img/chrome.png";
+import paint2 from "@/img/paint2.png";
+import figma from "@/img/figma.png";
+
+export const skills = {
+  "2024": {
+    "피그마": figma,
+    "에프터이펙트": after,
+    "프리미어 프로": premiere,
+    "포토샵": photoshop,
+    "윈도우 그림판": paint2,
+    "구글 크롬": chrome,
+  },
+  "2023": {
+    "에프터이펙트": after,
+    "프리미어 프로": premiere,
+    "포토샵": photoshop,
+    "윈도우 그림판": paint,
+    "구글 크롬": chrome,
+  },
+  "2020": {
+    "프리미어 프로": premiere,
+    "포토샵": photoshop,
+    "윈도우 그림판": paint,
+    "익스플로러": explorer,
+  },
+  "2017": {
+    "포토샵": photoshop,
+    "윈도우 그림판": paint,
+    "익스플로러": explorer,
+  },
+  "2009": {
+    "윈도우 그림판": paint,
+    "익스플로러": explorer,
+  },
+}
