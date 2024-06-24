15119de5b212501d987164010b39b03a85edb6e7
lee-sihun <lee.sihun040223@gmail.com>
2024-06-24 11:20:44 +0900

    design: 애니메이션 추가

--- components/SkillsList/index.tsx
+++ components/SkillsList/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, ReactNode } from "react";
 import { skills } from "./skills";
-import { motion, Variants } from "framer-motion";
+import { motion, Variants, useAnimationControls } from "framer-motion";
 
 interface YearButtonProps {
   active?: boolean;
@@ -22,6 +22,15 @@ function YearButton({ active, children, onClick }: YearButtonProps) {
   );
 }
 
+const variants: Variants = {
+  offscreen: { opacity: 0, y: 30 },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, ease: "easeInOut" },
+  },
+};
+
 const itemVariants: Variants = {
   hidden: { opacity: 0, scale: 0.8 },
   visible: {
@@ -41,13 +50,37 @@ export default function SkillsList() {
     setActiveYear(year);
   };
 
+  const buttonControls = useAnimationControls();
+  const skillControls = useAnimationControls();
+
+  const startButtonAnimation = () => {
+    buttonControls.start("onscreen");
+  };
+
+  const startSkillAnimation = () => {
+    skillControls.start("onscreen");
+  };
+
   return (
-    <section className="flex justify-center items-center h-screen px-[24px]">
+    <motion.section className="flex justify-center items-center h-screen px-[24px]">
       <div className="mx-auto max-w-[1200px] flex flex-wrap">
-        <h2 className="font-extrabold text-[54px] tracking-[-1.08px]">
+        <motion.h2
+          className="font-extrabold text-[54px] tracking-[-1.08px]"
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+          variants={variants}
+          onAnimationComplete={startButtonAnimation}
+        >
           사용하는 프로그램
-        </h2>
-        <div className="flex flex-wrap ml-[42px]">
+        </motion.h2>
+        <motion.div
+          className="flex flex-wrap ml-[42px]"
+          initial="offscreen"
+          animate={buttonControls}
+          variants={variants}
+          onAnimationComplete={startSkillAnimation}
+        >
           {years.map((year) => (
             <YearButton
               key={year}
@@ -57,35 +90,44 @@ export default function SkillsList() {
               {year}
             </YearButton>
           ))}
-        </div>
+        </motion.div>
         <motion.div
-          className="mx-auto flex flex-wrap items-center gap-[77px] h-[177px] rounded-[30px] bg-[#E9E9E9] px-[78px] overflow-hidden mt-[83px]"
-          layout
-          transition={{
-            duration: 0.5,
-            type: "spring",
-            stiffness: 60,
-            damping: 10,
-          }}
+          className="mx-auto"
+          initial="offscreen"
+          animate={skillControls}
+          variants={variants}
         >
-          {Object.entries(skills[activeYear]).map(([skillName, SkillIcon]) => (
-            <motion.div
-              key={skillName}
-              className="flex flex-col items-center"
-              variants={itemVariants}
-              initial="hidden"
-              animate="visible"
-            >
-              <div className="flex h-[85px] items-center mb-[16px]">
-                <SkillIcon />
-              </div>
-              <p className="relative font-medium text-[16px] text-[#313131] tracking-[-0.32px]">
-                {skillName}
-              </p>
-            </motion.div>
-          ))}
+          <motion.div
+            className="flex flex-wrap items-center gap-[77px] h-[177px] rounded-[30px] bg-[#E9E9E9] px-[78px] overflow-hidden mt-[83px]"
+            layout
+            transition={{
+              duration: 0.5,
+              type: "spring",
+              stiffness: 60,
+              damping: 10,
+            }}
+          >
+            {Object.entries(skills[activeYear]).map(
+              ([skillName, SkillIcon]) => (
+                <motion.div
+                  key={skillName}
+                  className="flex flex-col items-center"
+                  variants={itemVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  <div className="flex h-[85px] items-center mb-[16px]">
+                    <SkillIcon />
+                  </div>
+                  <p className="relative font-medium text-[16px] text-[#313131] tracking-[-0.32px]">
+                    {skillName}
+                  </p>
+                </motion.div>
+              )
+            )}
+          </motion.div>
         </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 }
