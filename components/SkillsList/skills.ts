27b94111a0238c11c5e804695dc9dd3c216b702c
lee-sihun <lee.sihun040223@gmail.com>
2024-06-24 10:01:52 +0900

    fix: 스킬 png 파일 svg로 수정

--- components/SkillsList/skills.ts
+++ components/SkillsList/skills.ts
@@ -1,41 +1,46 @@
-import explorer from "@/img/explorer.png";
-import paint from "@/img/paint.png";
-import photoshop from "@/img/photoshop.png";
-import premiere from "@/img/premiere.png";
-import after from "@/img/aftereffect.png";
-import chrome from "@/img/chrome.png";
-import paint2 from "@/img/paint2.png";
-import figma from "@/img/figma.png";
+import { StaticImageData } from 'next/image';
+import Figma from "@/svg/figma.svg";
+import After from "@/svg/after.svg";
+import Premiere from "@/svg/premiere.svg";
+import PhotoShop from "@/svg/photoshop.svg";
+import Paint from "@/svg/paint.svg";
+import Paint2 from "@/svg/paint2.svg";
+import Chrome from "@/svg/chrome.svg";
+import Explorer from "@/svg/explorer.svg";
 
-export const skills = {
+interface SkillSet {
+  [skillName: string]: React.ComponentType;
+}
+
+export const skills: { [year: string]: SkillSet } = {
   "2024": {
-    "피그마": figma,
-    "에프터이펙트": after,
-    "프리미어 프로": premiere,
-    "포토샵": photoshop,
-    "윈도우 그림판": paint2,
-    "구글 크롬": chrome,
+    "피그마": Figma,
+    "에프터이펙트": After,
+    "프리미어 프로": Premiere,
+    "포토샵": PhotoShop,
+    "윈도우 그림판": Paint2,
+    "구글 크롬": Chrome,
   },
   "2023": {
-    "에프터이펙트": after,
-    "프리미어 프로": premiere,
-    "포토샵": photoshop,
-    "윈도우 그림판": paint,
-    "구글 크롬": chrome,
+    "에프터이펙트": After,
+    "프리미어 프로": Premiere,
+    "포토샵": PhotoShop,
+    "윈도우 그림판": Paint,
+    "구글 크롬": Chrome,
   },
   "2020": {
-    "프리미어 프로": premiere,
-    "포토샵": photoshop,
-    "윈도우 그림판": paint,
-    "익스플로러": explorer,
+    "프리미어 프로": Premiere,
+    "포토샵": PhotoShop,
+    "윈도우 그림판": Paint,
+    "익스플로러": Explorer,
   },
   "2017": {
-    "포토샵": photoshop,
-    "윈도우 그림판": paint,
-    "익스플로러": explorer,
+    "포토샵": PhotoShop,
+    "윈도우 그림판": Paint,
+    "익스플로러": Explorer,
   },
   "2009": {
-    "윈도우 그림판": paint,
-    "익스플로러": explorer,
+    "윈도우 그림판": Paint,
+    "익스플로러": Explorer,
   },
 }
