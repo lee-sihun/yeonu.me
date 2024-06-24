a53078961cd3cc35648f7499c96426b6fe3b615d
lee-sihun <lee.sihun040223@gmail.com>
2024-06-24 17:27:43 +0900

    feat: html lang="kr"

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="kr">
       <body className={pretendard.className}>
         <Header />
         <main>{children}</main>
