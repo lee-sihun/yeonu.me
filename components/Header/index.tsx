072dcb5788f449f662d62602f2cd5b71004b138a
lee-sihun <lee.sihun040223@gmail.com>
2024-06-25 18:52:27 +0900

    design: header 반응형

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,13 +6,15 @@ export default function Header() {
 
   return (
     <header className="headerglass fixed top-0 left-0 w-full z-[100] flex justify-between items-center min-h-[46px] border-b-[1px] border-[#e8e8e8] border-solid">
-      <span className="font-bold text-[18px] text-[#111111] ml-[17px]">
+      <span className="font-bold text-[18px] text-[#111111] ml-[17px] max-sm:hidden">
         Introducing!
       </span>
-      <div className="text-[14px] text-[#111111]">im@yeonu.me</div>
+      <div className="text-[14px] text-[#111111] max-sm:mx-auto">
+        im@yeonu.me
+      </div>
       <time
         dateTime={today.toISOString()}
-        className="text-[14px] text-[#1581FF] mr-[17px]"
+        className="text-[14px] text-[#1581FF] mr-[17px] max-sm:hidden"
       >
         {formattedDate}
       </time>
