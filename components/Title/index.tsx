1b55d70e4619f16d0993bef7bcfe24c01c792a9a
lee-sihun <lee.sihun040223@gmail.com>
2024-06-25 21:30:46 +0900

    design: title 반응형 개선

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -6,13 +6,22 @@ import logoJson from "@/json/logo.json";
 
 export default function Title() {
   const lottieRef = useRef(null);
-  const [description, setDescription] = useState("당신을 위한 디자인 \n ㅤ");
+  const [description, setDescription] = useState(
+    <span>
+      당신을 위한 디자인
+      <br />ㅤ
+    </span>
+  );
 
   const handleEnterFrame = (frame: number) => {
-    const roundedFrame = Math.round(frame); // 프레임 값을 정수로 반올림
+    const roundedFrame = Math.round(frame);
     if (roundedFrame === 145) {
       setDescription(
-        "개성과 가치를 시각적으로 구현하며 \n 일관적이면서도 감성적으로 연결할 수 있는 비주얼 아이덴티티를 창조합니다."
+        <span>
+          개성과 가치를 시각적으로 구현하며 <br className="max-sm:hidden" />
+          일관적이면서도 <br className="hidden max-sm:inline" /> 감성적으로
+          연결할 수 있는 비주얼 아이덴티티를 창조합니다.
+        </span>
       );
     }
   };
@@ -43,10 +52,10 @@ export default function Title() {
           ref={lottieRef}
           onEnterFrame={handleEnterFrame}
         />
-        <div className="mt-[-32px] text-center text-[24px] max-sm:text-[14px] text-[#777777] tracking-[-0.52px] max-sm:tracking-[-0.28px] leading-[34px] max-sm:leading-[22px] whitespace-pre-line flex items-start">
+        <div className="mt-[-32px] text-center text-[24px] max-sm:text-[16px] text-[#777777] tracking-[-0.52px] max-sm:tracking-[-0.28px] leading-[34px] max-sm:leading-[22px] whitespace-pre-line flex items-start">
           <AnimatePresence mode="wait">
             <motion.p
-              key={description} // description이 바뀔 때마다 새로운 key 할당
+              // key={description} // description이 바뀔 때마다 새로운 key 할당
               initial="hidden"
               animate="visible"
               exit="hidden"
