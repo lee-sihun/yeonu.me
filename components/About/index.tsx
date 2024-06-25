7d14a0bcfb71a7b9808aab99343b483872bcd8cb
lee-sihun <lee.sihun040223@gmail.com>
2024-06-25 16:54:51 +0900

    feat: 애니메이션 추가

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -2,10 +2,12 @@
 import { useRef } from "react";
 import { motion } from "framer-motion";
 import ProfileCard from "./ProfileCard";
+import ArrowSvg from "@/svg/arrow-bottom.svg";
 
 export default function About() {
   const textRef = useRef<HTMLDivElement>(null);
   const cardRef = useRef<HTMLDivElement>(null);
+  const descRef = useRef<HTMLDivElement>(null);
 
   const divVariants = {
     hidden: { clipPath: "polygon(0 0, 0% 0, 0% 100%, 0% 100%)" },
@@ -19,8 +21,9 @@ export default function About() {
   };
 
   const cardAnimation = () => {
-    cardRef.current?.classList.add('active');
-    textRef.current?.classList.add('active');
+    cardRef.current?.classList.add("active");
+    textRef.current?.classList.add("active");
+    descRef.current?.classList.add("active");
   };
 
   return (
@@ -41,6 +44,34 @@ export default function About() {
       <div ref={cardRef} className="about-card">
         <ProfileCard />
       </div>
+      <article ref={descRef} className="about-desc">
+        <div className="flex flex-col items-center">
+          <p className="text-[30px] font-extrabold text-[#777777] tracking-[-0.32px]">
+            CONTACT <span className="text-[#3F3F3F]">. ME!</span>
+          </p>
+          <ArrowSvg className="mt-[30px]" />
+          <span className="mt-[38px] text-center text-[26px] font-medium text-[#777777] tracking-[-0.52px] leading-[32px] whitespace-nowrap">
+            앗, 제 소개를 깜빡했군요.
+            <br /> 안녕하세요!{" "}
+            <span className="text-[#3F3F3F] underline">
+              비주얼 아이덴티티 디자이너{" "}
+            </span>
+            연우입니다. <br />
+            <br />
+            단순히 눈에 보이는 <span className="text-[#3F3F3F]">디자인</span>을
+            넘어 <span className="text-[#3F3F3F]">디테일</span>에 대한{" "}
+            <span className="text-[#3F3F3F]">집착</span>과
+            <br />
+            <span className="text-[#3F3F3F]">창의적인 접근</span>을 통해{" "}
+            <span className="text-[#3F3F3F]">브랜드</span>가 전달하고자 하는{" "}
+            <span className="text-[#3F3F3F]">가치</span>와{" "}
+            <span className="text-[#3F3F3F]">이야기</span>를
+            <br />
+            <span className="text-[#3F3F3F]">시각적</span>으로 풀어내는 것에
+            중점을 둡니다.
+          </span>
+        </div>
+      </article>
     </section>
   );
 }
