8784775d3f7d1613cb45fa6e2aaa6827ce099b5e
lee-sihun <lee.sihun040223@gmail.com>
2024-06-25 13:56:31 +0900

    feat: Card & Text 애니메이션

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { useRef } from "react";
 import { motion } from "framer-motion";
 import ProfileCard from "./ProfileCard";
 
 export default function About() {
+  const textRef = useRef<HTMLDivElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+
   const divVariants = {
     hidden: { clipPath: "polygon(0 0, 0% 0, 0% 100%, 0% 100%)" },
     visible: {
@@ -14,20 +18,29 @@ export default function About() {
     },
   };
 
+  const cardAnimation = () => {
+    cardRef.current?.classList.add('active');
+    textRef.current?.classList.add('active');
+  };
+
   return (
-    <section className="flex flex-col justify-center items-center h-screen">
+    <section className="relative flex flex-col justify-center items-center h-screen overflow-hidden">
       <motion.div
-        className="overflow-hidden"
+        ref={textRef}
+        className="overflow-hidden about-text"
         variants={divVariants}
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
+        onAnimationComplete={cardAnimation}
       >
         <h2 className="font-extrabold text-[80px] tracking-[-1.6px]">
           근데 누구..?
         </h2>
       </motion.div>
-      <ProfileCard />
+      <div ref={cardRef} className="about-card">
+        <ProfileCard />
+      </div>
     </section>
   );
 }
