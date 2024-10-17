360755ca79d7b7c47bb835d5605497da43cfd367
yeonu-me <yeonu@eunyeon.com>
2024-10-18 01:07:47 +0900

    Update videos.ts

--- components/Gallery/videos.ts
+++ components/Gallery/videos.ts
@@ -4,18 +4,14 @@ export const videos = {
 	alt: "video1",
   },
   2: {
-	src: "/video/img2.mp4",
-	alt: "video2",
-  },
-  3: {
 	src: "/video/img3.mp4",
 	alt: "video3",
   },
-  4: {
+  3: {
 	src: "/video/img4.mp4",
 	alt: "video4",
   },
-  5: {
+  4: {
 	src: "/video/img5.mp4",
 	alt: "video5",
   },
