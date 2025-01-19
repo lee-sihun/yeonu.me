410010f95404db97f418c0d00fe794f4dd36750d
연우 <yeonu@eunyeon.com>
2025-01-20 01:23:22 +0900

    Update videos.ts

--- components/Gallery/videos.ts
+++ components/Gallery/videos.ts
@@ -1,21 +1,21 @@
 export const videos = {
-  5: {
+  1: {
     src: "/video/img6.mp4",
     alt: "video6",
   },
-  1: {
+  2: {
     src: "/video/img1.mp4",
     alt: "video1",
   },
-  2: {
+  3: {
     src: "/video/img3.mp4",
     alt: "video3",
   },
-  3: {
+  4: {
     src: "/video/img4.mp4",
     alt: "video4",
   },
-  4: {
+  5: {
     src: "/video/img5.mp4",
     alt: "video5",
   },
