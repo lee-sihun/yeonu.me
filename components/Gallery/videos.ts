ad5fe036c3a1460810e552ef235492771f1e8ea0
연우 <yeonu@eunyeon.com>
2025-01-20 00:49:23 +0900

    Update videos.ts

--- components/Gallery/videos.ts
+++ components/Gallery/videos.ts
@@ -1,18 +1,22 @@
 export const videos = {
+  5: {
+    src: "/video/img6.mp4",
+    alt: "video6",
+  },
   1: {
-	src: "/video/img1.mp4",
-	alt: "video1",
+    src: "/video/img1.mp4",
+    alt: "video1",
   },
   2: {
-	src: "/video/img3.mp4",
-	alt: "video3",
+    src: "/video/img3.mp4",
+    alt: "video3",
   },
   3: {
-	src: "/video/img4.mp4",
-	alt: "video4",
+    src: "/video/img4.mp4",
+    alt: "video4",
   },
   4: {
-	src: "/video/img5.mp4",
-	alt: "video5",
+    src: "/video/img5.mp4",
+    alt: "video5",
   },
 };
