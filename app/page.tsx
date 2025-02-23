0dceef24b2123d3ae60ddfddd33f0b8c9f0cc2ba
lee-sihun <lee.sihun040223@gmail.com>
2025-02-23 23:29:26 +0900

    feat: 페이지별 테마 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
+"use client"
 import Title from "@/components/Title";
 import Gallery from "@/components/Gallery";
 import About from "@/components/About";
 import SkillsList from "@/components/SkillsList";
 import Contact from "@/components/Contact";
 import Header from "@/components/Header";
+import { ThemeProvider } from "next-themes";
 
 export default function Home() {
   return (
-    <>
+    <ThemeProvider
+      attribute="class"
+      enableSystem={false}
+      forcedTheme="light"
+      storageKey="main-theme"
+    >
       <Header />
       <Title />
       <Gallery />
       <About />
       <SkillsList />
       <Contact />
-    </>
+    </ThemeProvider>
   );
 }
--- components/Blog/Provider.tsx
+++ components/Blog/Provider.tsx
@@ -3,7 +3,12 @@ import { ThemeProvider } from "next-themes";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
+    <ThemeProvider
+      enableSystem={true}
+      attribute="class"
+      storageKey="blog-theme"
+      defaultTheme="system"
+    >
       {children}
     </ThemeProvider>
   );
